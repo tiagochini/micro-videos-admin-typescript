d9596f062e619067476c868e048e1568f2c5f698
Tiago Chini <tiago.chini@hotmail.com>
2022-07-04 22:27:22 +0000

    feat: add shared and immutable value object

--- src/_Shared/Domain/ValueObjects/unique-entity-id.vo.spec.ts
+++ src/_Shared/Domain/ValueObjects/unique-entity-id.vo.spec.ts
@@ -1,4 +1,4 @@
-import InvalidUuidError from "../Errors/invalid-uuid.error";
+import InvalidUuidError from "../../Errors/invalid-uuid.error";
 import UniqueEntityId from "./unique-entity-id.vo";
 import { validate as uuidValidator } from "uuid";
 
@@ -14,32 +14,26 @@ describe('Entity UniqueEntityId', () => {
 
     it('test fail of constructor is invalid uuid', () => {
         const validateSpy = spyValidateMethod();
-        expect(() => {
-            new UniqueEntityId('fake_uuid');
-        }).toThrow(new InvalidUuidError());
-        expect(validateSpy).toHaveBeenCalled();
-        expect(() => {
-            new UniqueEntityId('fake_uuid');
-        }).toThrowError('ID must be a valid UUID');
+        expect(() =>  new UniqueEntityId('fake_uuid')).toThrow(new InvalidUuidError());
         expect(validateSpy).toHaveBeenCalled();
     });
 
     it('should accept a uuid passed in constructor', () => {
         const validateSpy = spyValidateMethod();
         const id = new UniqueEntityId('b826de14-8c48-4c55-b140-2ba0496e32c4');
-        expect(id.id).not.toBeNull();
+        expect(id.value).not.toBeNull();
         expect(id).toBeInstanceOf(UniqueEntityId);
-        expect(id.id.length).toBe(36);
+        expect(id.value.length).toBe(36);
         expect(validateSpy).toHaveBeenCalled();
     });
 
     it('should accept a uuid not passed in constructor', () => {
         const validateSpy = spyValidateMethod();
         const id = new UniqueEntityId();
-        expect(id.id).not.toBeNull();
+        expect(id.value).not.toBeNull();
         expect(id).toBeInstanceOf(UniqueEntityId);
-        expect(id.id.length).toBe(36);
-        expect(() => uuidValidator(id.id)).toBeTruthy();
+        expect(id.value.length).toBe(36);
+        expect(() => uuidValidator(id.value)).toBeTruthy();
         expect(validateSpy).toHaveBeenCalled();
     });
 
---
+++ src/_Shared/Domain/ValueObjects/value-object.spec.ts
@@ -0,0 +1,61 @@
+import { values } from "lodash";
+import ValueObject from "./value-objects";
+
+class StubValueObject extends ValueObject {
+
+}
+
+
+describe('ValueObjects Unit Test', () => {
+
+    it('should set value', () => {
+        const vo = new StubValueObject('String Value');
+        expect(vo.value).toBe('String Value');
+
+        const vo1 = new StubValueObject({ prop1: 'Value1' });
+        expect(vo1 + "").toStrictEqual(JSON.stringify({ prop1: 'Value1' }));
+    });
+
+    it('should return string representation of value', () => {
+        const date = new Date();
+        let arrange = [
+            { received: "", expected: "" },
+            { received: "Fake teste", expected: "Fake teste" },
+            { received: 0, expected: "0" },
+            { received: 1, expected: "1" },
+            { received: 5, expected: "5" },
+            { received: false, expected: "false" },
+            { received: true, expected: "true" },
+            { received: date, expected: date.toString() },
+            { received: { prop1: 'Value1' }, expected: JSON.stringify({ prop1: 'Value1' }) },
+        ];
+
+        arrange.forEach(value => {
+            const vo = new StubValueObject(value.received);
+            expect(vo + "").toBe(value.expected);
+        });
+    });
+
+    it("should be a immutable object", () => {
+        const obj = {
+            prop1: "Value1",
+            deep: {
+                prop2: "value2",
+                prop3: new Date()
+            }
+        };
+        
+        const vo = new StubValueObject(obj);
+
+        expect(() => {
+            (vo as any).value.prop1 = "Test";
+        }).toThrow();
+
+        expect(() => {
+            (vo as any).values.deep.prop2 = "Test";
+        }).toThrow();
+
+        expect(vo.value.deep.prop3).toBeInstanceOf(Date);
+    });
+
+});
